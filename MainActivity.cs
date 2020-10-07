58f31f7ab2d795a1f861a1fc16ac066210f237c2
neildandekar <Neil.Dandekar@outlook.com>
2020-10-07 11:00:24 +0530

    Add project files.

---
+++ MainActivity.cs
@@ -0,0 +1,75 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content.Res;
+using System.IO;
+using System;
+
+namespace TrialTeacher
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private Button buttonOk;
+        private TextView textView1;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            convert();
+            buttonOk = FindViewById<Button>(Resource.Id.buttonOk);
+            textView1 = FindViewById<TextView>(Resource.Id.textView1);
+            textView1.Text = "Blah, blah ,blah";
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        public void convert()
+        {
+            string content;
+            AssetManager assets = this.Assets;
+            using (StreamReader sr = new StreamReader(assets.Open("DBCOPY.txt")))
+            {
+                content = sr.ReadToEnd();
+            }
+            var sqliteFilename = "hottots.db";
+            string documentsDirectoryPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            var path = Path.Combine(documentsDirectoryPath, sqliteFilename);
+
+            // This is where we copy in our pre-created database
+            if (!File.Exists(path) || content.Equals("FORCE"))
+            {
+                try
+                {
+                    AssetManager asset = this.Assets;
+                    using (var binaryReader = new BinaryReader(asset.Open(sqliteFilename)))
+                    {
+                        using (var binaryWriter = new BinaryWriter(new FileStream(path, FileMode.Create)))
+                        {
+                            byte[] buffer = new byte[2048];
+                            int length = 0;
+                            while ((length = binaryReader.Read(buffer, 0, buffer.Length)) > 0)
+                            {
+                                binaryWriter.Write(buffer, 0, length);
+                            }
+                        }
+                        Toast.MakeText(Application.Context, "Converted DB", ToastLength.Short).Show();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Toast.MakeText(Application.Context, "Could not convert the db", ToastLength.Short).Show();
+                    return;
+                }
+            }
+            return;
+        }
+    }
+}
---
+++ SQLiteDatabaseOperations.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Data;
+
+namespace BitmapToSqlite
+{
+    class SQLiteDatabaseOperations
+    {
+        public SQLiteDatabaseOperations()
+        {
+            try
+            {
+                string cs = @"URI=file:d:\hottots.db";
+                SQLiteConnection conn = new SQLiteConnection(cs);
+                string sqlQuery = "SELECT * FROM thsNames";
+                SQLiteCommand cmd = new SQLiteCommand(sqlQuery, conn);                                
+                SQLiteDataReader reader = cmd.ExecuteReader();
+                List<String> records = new List<String>();
+                 while (reader.Read())
+                {
+                    records.Add(reader.GetInt32(0).ToString() + "-" + reader.GetString(1));
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public int SaveImageRecord(byte[] ba, string name)
+        {
+            try { 
+            string cs = @"URI=file:d:\hottots.db";
+            SQLiteConnection conn = new SQLiteConnection(cs);
+            conn.Open();
+            string sqlQuery = "insert into Images (name,image)values(@a,@d)";
+            SQLiteCommand cmd = new SQLiteCommand(sqlQuery, conn);
+            cmd.Parameters.AddWithValue("@a", name);
+            cmd.Parameters.AddWithValue("@d", ba);
+            return cmd.ExecuteNonQuery();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return -1;
+            }
+
+        }
+        public int SaveSoundRecord(byte[] ba,string name)
+        {
+            try
+            {
+                string cs = @"URI=file:d:\hottots.db";
+                SQLiteConnection conn = new SQLiteConnection(cs);
+                conn.Open();
+                string sqlQuery = "insert into Sounds (sound,name)values(@s,@n)";
+                SQLiteCommand cmd = new SQLiteCommand(sqlQuery, conn);
+                cmd.Parameters.AddWithValue("@s", ba);
+                cmd.Parameters.AddWithValue("@n", name);
+                return cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return -1;
+            }
+
+        }
+        public byte[] getImage(string key)
+        {
+            try
+            {
+                string cs = @"URI=file:d:\hottots.db";
+                SQLiteConnection conn = new SQLiteConnection(cs);
+                conn.Open();
+                string sqlQuery = "SELECT image FROM Images where name = @n";
+                SQLiteCommand cmd = new SQLiteCommand(sqlQuery, conn);
+                cmd.Parameters.AddWithValue("@n", key);
+                SQLiteDataReader reader = cmd.ExecuteReader();
+                List<byte[]> records = new List<byte[]>();
+                while (reader.Read())
+                {
+                    byte[] imageArray = new byte[25000]; 
+                    reader.GetBytes(0,0,imageArray,0,25000);
+                    records.Add(imageArray);
+                    
+                }
+                return records[0];
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+        public byte[] getSound(string key)
+        {
+         try
+            {
+                string cs = @"URI=file:d:\hottots.db";
+                SQLiteConnection conn = new SQLiteConnection(cs);
+                conn.Open();
+                string sqlQuery = "SELECT sound FROM sounds where name = @n ";
+                SQLiteCommand cmd = new SQLiteCommand(sqlQuery, conn);
+                cmd.Parameters.AddWithValue("@n", key);
+                SQLiteDataReader reader = cmd.ExecuteReader();
+                List<byte[]> records = new List<byte[]>();
+                while (reader.Read())
+                {
+                    byte[] soundArray = new byte[60000];
+                    reader.GetBytes(0, 0, soundArray, 0, 60000);
+                    records.Add(soundArray);
+                }
+                return records[0];
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+}
+
+    }
+}
