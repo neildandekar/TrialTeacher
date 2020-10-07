4083a8bae1a29b66be899f07de63be166b318023
neildandekar <Neil.Dandekar@outlook.com>
2020-10-07 16:37:50 +0530

    Working on a proto

--- L1.cs
+++ L1.cs
@@ -1,20 +1,27 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Widget;
+using Microsoft.Azure.WebJobs.Host.Bindings;
+using System.Collections.Generic;
 
 namespace TrialTeacher
 {
     [Activity(Label = "L1")]
     public class L1 : Activity
-    {
-        private TextView tv1;
+    { 
+        public IList <string> words { get; private set; }
+        private ListView lv1;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.L1);
-            tv1 = FindViewById<TextView>(Resource.Id.textView1);
-            tv1.Text = "Reached here";
-            // Create your application here
+            lv1 = FindViewById<ListView>(Resource.Id.listView1);
+            words = new List<string>();
+            words.Add("कमल");
+            words.Add("विमल");
+            words.Add("धवल");
+            ArrayAdapter adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, words);
+            lv1.Adapter = adapter;
         }
     }
 }
